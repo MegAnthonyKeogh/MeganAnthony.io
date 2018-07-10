da803c57c55f163e42b083264213827022b18627
Megan Anthony <megananthony@Megans-MacBook-Air.local>
2018-07-10 14:58:07 -0400

    video background

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,17 @@
-alert("welcome!");
+function toggleMute() {
+
+    var video=document.getElementById("videoId");
+  
+    if(video.muted){
+      video.muted = false;
+    } else {
+      debugger;
+      video.muted = true;
+      video.play()
+    }
+  
+  }
+  
+ /* $(document).ready(function(){
+    setTimeout(toggleMute,3000);
+  })
